24bf8b9ae74ff3e5784f9ced01f70cc14fbd6657
PIYUSH MISHRA <piyushhmishraaa@gmail.com>
2026-02-08 15:20:26 +0530

    update profile function added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 
 import express from "express"
 const app = express();
+import cookieParser from "cookie-parser"
 import dotenv from "dotenv"
 import path from "path"
 import authRoutes from"./routes/auth.route.js"
@@ -13,6 +14,7 @@ const __dirname = path.resolve();
 const PORT = process.env.PORT||5000;
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.use("/api/auth",authRoutes)
 if(process.env.NODE_ENV==="production"){
